09222ebc14d48906c0fc3363d9cc07dd1b576ff9
dinesh-dev <sathasiv001@knights.gannon.edu>
2015-04-14 17:01:29 -0400

    New_file_structure

---
+++ Main.java
@@ -0,0 +1,12 @@
+package junitexample;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Triangle t = new Triangle("12", "12", "12");
+	        System.out.println("triangle is:"+t.determineTriangleType());
+	}
+
+}
